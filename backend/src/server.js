ea9116d2424162e13850c3a16db87fdcacf0b39b
Magic-Divya <divyadiya3336@gmail.com>
2025-11-25 13:12:31 +0530

    Fixed folder structure

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+
+// Routes
 import authRoutes from "./routes/authRoutes.js";
 import studentRoutes from "./routes/studentRoutes.js";
 import courseRoutes from "./routes/courseRoutes.js";
@@ -9,19 +11,27 @@ import courseRoutes from "./routes/courseRoutes.js";
 dotenv.config();
 
 const app = express();
+
+// Middlewares
 app.use(express.json());
 app.use(cors());
 
-// DB connect
+// DB Connection
 connectDB();
 
+// Root Route
 app.get("/", (req, res) => {
   res.send("Student Registration Portal API is running...");
 });
 
+// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/students", studentRoutes);
 app.use("/api/courses", courseRoutes);
 
+// Render PORT fix
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
